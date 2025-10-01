966e2f27d41aa6467d0ccb0f7e7f8aba2c977cd7
Ben Taft <ben.taft@career2cradle.org>
2025-10-01 11:08:37 -0500

    update how postsecondary completers are wrangled

--- R/wrangle_local_postsecondary_completers.R
+++ R/wrangle_local_postsecondary_completers.R
@@ -1,62 +1,67 @@
-#' Wrangle raw IPEDS graduation data for local schools into the local 150% postsecondary completion rate
-#'
-#' @param .DATA a data frame with GR data a la [`hercipeds::read_graduation_rates()`]
-#' @param .unit_ids the UNITID codes for all of the local schools
-#'
-#' @return a data frame ready for [`to_wide_cia_format()`]
-#' @export
-wrangle_local_postsecondary_completers <- function(.DATA, .unit_ids) {
-    .DATA |>
-        dplyr::inner_join(
-            tibble::tibble(UNITID = .unit_ids),
-            by = "UNITID"
+.helper <- function(.nested_data, .key, ...) {
+    .nested_data |>
+        dplyr::select(
+            tidyselect::all_of(c("Index", .key))
         ) |>
-        dplyr::inner_join(
-            herccia::SEMI_JOINS_FOR_IPEDS,
-            by = c("Status", "Relative Time", "Credential Earned")
+        tidyr::unnest(
+            tidyselect::all_of(.key)
         ) |>
         dplyr::filter(
-            .data$`Credential Sought` != "Any"
+            ...
         ) |>
-        dplyr::inner_join(
-            herccia::GLOSSARY_OF_POPS_FOR_IPEDS,
-            by = c("Sex", "Race/Ethnicity")
+        dplyr::count(
+            .data$Index,
+            wt = .data$Count,
+            name = .key
         )
 }
 
+#' Wrangle raw IPEDS graduation data for local schools into the local 150% postsecondary completion rate
+#'
+#' @param .DATA a data frame with GR data a la [`hercipeds::read_graduates()`]
+#'
+#' @return a data frame ready for [`to_wide_cia_format()`]
+#' @export
+wrangle_local_postsecondary_completers <- function(.DATA) {
+    .tmp <- dplyr::mutate(.DATA,
+                          Index = dplyr::row_number())
 
-#|>
-#    dplyr::inner_join(
-#        herccia::GLOSSARY_OF_POPS_FOR_IPEDS,
-#        by = c("Sex", "Race/Ethnicity")
-#    ) |>
-#    dplyr::semi_join(
-#        herccia::SEMI_JOINS_FOR_IPEDS,
-#        by = c("Status", "Relative Time", "Credential Earned")
-#    ) |>
-#    dplyr::filter(
-#        .data$`Credential Sought` != "Any"
-#    ) |>
-#    dplyr::summarize(
-#        Count = sum(.data$Count, na.rm = TRUE),
-#        .by = c("UNITID",
-#                "Year",
-#                "Status",
-#                "Gender",
-#                "Population")
-#    ) |>
-#    tidyr::pivot_wider(
-#        names_from = "Status",
-#        values_from = "Count",
-#        values_fill = 0
-#    ) |>
-#    dplyr::left_join(
-#        herccia::GLOSSARY_OF_POPS_FOR_COMPS,
-#        by = c("Gender", "Population")
-#    ) |>
-#    dplyr::rename(
-#        START_YEAR = "Year",
-#        Students = "Adjusted Cohort",
-#        Completers = "Completed"
-#    )
-#
+    .cohort <- .helper(.tmp,
+                       "Cohort",
+                       .data$Statistic == "Adjusted cohort")
+
+    .completers <- .helper(.tmp,
+                           "Completers",
+                           .data$`Award Level` == "Any degree",
+                           .data$`Time to Award` == "<=150%")
+
+    .tmp |>
+        dplyr::select(
+            !tidyselect::any_of(c("Cohort", "Completers"))
+        ) |>
+        dplyr::filter(
+            .data$`Award Sought` == "Any degree"
+        ) |>
+        dplyr::rename(
+            `Race/Ethnicity` = "Population"
+        ) |>
+        dplyr::inner_join(
+            herccia::GLOSSARY_OF_POPS_FOR_IPEDS
+        ) |>
+        dplyr::inner_join(
+            .cohort,
+            by = "Index"
+        ) |>
+        dplyr::inner_join(
+            .completers,
+            by = "Index"
+        ) |>
+        dplyr::summarize(
+            N = sum(.data$Completers, na.rm = TRUE),
+            D = sum(.data$Cohort, na.rm = TRUE),
+            .by = c("Year",
+                    "Population",
+                    "Gender"
+            )
+        )
+}
--- data-raw/GLOSSARY_OF_POPS_FOR_IPEDS.R
+++ data-raw/GLOSSARY_OF_POPS_FOR_IPEDS.R
@@ -13,9 +13,11 @@ GLOSSARY_OF_POPS_FOR_IPEDS <- dplyr::cross_join(
         "American Indian or Alaska Native", "American Indian or Alaska Native",
         "Asian",                            "Asian",
         "Black",                            "Black or African American",
+        "Direct Subsidized Loan",           "Low Income",
         "Hispanic",                         "Hispanic or Latine",
         "Multiracial",                      "Multi-Racial",
         "Pacific Islander",                 "Native Hawaiian or Pacific Islander",
+        "Pell Grant",                       "Low Income",
         "Unknown",                          "Other Race",
         "White",                            "White"
     )
